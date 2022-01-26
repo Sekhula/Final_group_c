5d71cd9b70400d59e8f5d52fd30eb5866bf97a57
Tebalelo Sekhula <bokamoso@Tebalelos-MacBook-Air.local>
2022-01-26 23:55:24 +0200

    Added comments in the backend and the product service

--- Frontend/src/app/services/product-service.service.ts
+++ Frontend/src/app/services/product-service.service.ts
@@ -18,33 +18,36 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductServiceService {
+
   private baseURL = 'http://localhost:3000/api/products';
+
   constructor(private httpClient: HttpClient) { }
 
+  //Geting all products from the db
   readAll(): Observable<any> {
     return this.httpClient.get(this.baseURL);
   }
-
+  //Getting a product by speciefied ID
   read(id: number): Observable<any> {
     return this.httpClient.get(`${this.baseURL}/${id}`);
   }
-
+//Adding a product to the db
   create(data: any): Observable<any> {
     return this.httpClient.post(this.baseURL, data);
   }
-
+//Update or modify the specified product
   update(id: number, data: any): Observable<any> {
     return this.httpClient.put(`${this.baseURL}/${id}`, data);
   }
-
+//Delete a specified product from the db
   delete(id: number): Observable<any> {
     return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
-
+//Delete all products in the db
   deleteAll(): Observable<any> {
     return this.httpClient.delete(this.baseURL);
   }
-
+//Get the product by searching
   searchByName(name: any): Observable<any> {
     return this.httpClient.get(`${this.baseURL}?name=${name}`);
   }
