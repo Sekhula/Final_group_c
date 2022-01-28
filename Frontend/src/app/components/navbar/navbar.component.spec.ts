ce5834264261b79837831b544ffd71f3474b0e67
Letlhogonolo01 <academy_learner@Academy-learners-MacBook-Air-2.local>
2022-01-28 17:03:13 +0200

    home page

--- Frontend/src/app/components/navbar/navbar.component.spec.ts
+++ Frontend/src/app/components/navbar/navbar.component.spec.ts
@@ -18,8 +18,8 @@ describe('NavbarComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
+//
+  it('should create ', () => {
     expect(component).toBeTruthy();
   });
 });
